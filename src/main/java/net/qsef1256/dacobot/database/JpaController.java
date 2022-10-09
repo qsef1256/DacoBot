27d957a8c1b376d2c3ffa5d339091fdbad56c45e
qsef1256 <qsef1256@naver.com>
2022-10-09 21:31:05 +0900

    remove JpaController#getSessionFactoryFromJPA

--- src/main/java/net/qsef1256/dacobot/database/JpaController.java
+++ src/main/java/net/qsef1256/dacobot/database/JpaController.java
@@ -5,8 +5,6 @@ import jakarta.persistence.EntityManagerFactory;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 
 import static net.qsef1256.dacobot.DacoBot.logger;
 
@@ -21,7 +19,7 @@ public class JpaController {
         try {
             entityManagerFactory = new JpaEntityManagerFactory().getEntityManagerFactory();
             threadLocal = new ThreadLocal<>();
-        } catch (final Exception e) {
+        } catch (Exception e) {
             logger.error(e.getMessage());
             throw new ExceptionInInitializerError(e);
         }
@@ -48,18 +46,6 @@ public class JpaController {
             entityManager.close();
             threadLocal.remove();
         }
-    }
-
-    /**
-     * @return Hibernate SessionFactory
-     * @deprecated use JPA EntityManager instead
-     */
-    @Deprecated
-    public static SessionFactory getSessionFactoryFromJPA() {
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        Session session = entityManager.unwrap(Session.class);
-        session.getSessionFactory().openSession(); // 원래 쓰고 닫아야 하지만 DaoCommonHibernateImpl 은 getCurrentSession() 을 사용하므로 당장은 필요치 않음
-        return session.getSessionFactory();
     }
 
     public static void shutdown() {
