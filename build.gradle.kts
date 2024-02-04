96070e4a1b38074584e15c1da8bd3e6290585dc1
qsef1256 <qsef1256@naver.com>
2024-02-04 19:57:27 +0900

    setup refreshVersions

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,51 +34,60 @@ repositories {
 }
 
 dependencies {
-    implementation("ch.qos.logback:logback-classic:1.4.14")
-    implementation("ch.qos.logback:logback-core:1.4.14")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
-    implementation("com.github.ben-manes.caffeine:caffeine:3.1.8")
-    implementation("com.google.code.gson:gson:2.10.1")
-    implementation("com.google.guava:guava:32.1.2-jre")
-    implementation("com.zaxxer:HikariCP:5.1.0")
-    implementation("com.jayway.jsonpath:json-path:2.9.0")
-    implementation("commons-beanutils:commons-beanutils:1.9.4") {
-        exclude(group = "commons-collections", module = "commons-collections")
-    }
-    implementation("commons-codec:commons-codec:1.16.0")
-    implementation("io.smallrye:jandex:3.1.3")
-    implementation("jakarta.el:jakarta.el-api:5.0.1")
+    // Core
     implementation("net.dv8tion:JDA:$jdaVersion")
+    implementation("pw.chew:jda-chewtils:2.0-SNAPSHOT")
     implementation("net.qsef1256:DiaLib:1.1.8")
+    // Technical
+    implementation("org.glassfish:jakarta.el:5.0.0-M1")
+    implementation("jakarta.el:jakarta.el-api:5.0.1")
+    implementation("io.smallrye:jandex:3.1.3")
+    implementation("com.jayway.jsonpath:json-path:2.9.0")
+    // Apache
     implementation("org.apache.commons:commons-collections4:4.4")
     implementation("org.apache.commons:commons-configuration2:2.9.0")
     implementation("org.apache.commons:commons-dbcp2:2.9.0")
     implementation("org.apache.commons:commons-lang3:3.13.0")
     implementation("org.apache.commons:commons-text:1.10.0")
     implementation("org.apache.httpcomponents.client5:httpclient5:5.3-alpha1")
-    implementation("org.apache.maven:maven-model:4.0.0-alpha-7")
-    implementation("org.dom4j:dom4j:2.1.4")
-    implementation("org.glassfish:jakarta.el:5.0.0-M1")
+    implementation("commons-beanutils:commons-beanutils:1.9.4") {
+        exclude(group = "commons-collections", module = "commons-collections")
+    }
+    implementation("commons-codec:commons-codec:1.16.0")
+    // Functional
+    implementation("org.mariuszgromada.math:MathParser.org-mXparser:5.2.1")
+    // Utility
     implementation("org.jetbrains:annotations:24.0.1")
+    implementation("org.projectlombok:lombok:1.18.28")
+    implementation("org.dom4j:dom4j:2.1.4")
+    implementation("org.reflections:reflections:0.10.2")
+    implementation("com.github.ben-manes.caffeine:caffeine:3.1.8")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.google.guava:guava:32.1.2-jre")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+    // Database
+    implementation("com.zaxxer:HikariCP:5.1.0")
     implementation("org.hibernate:hibernate-core:6.4.3.Final")
     implementation("org.hibernate:hibernate-hikaricp:6.4.3.Final")
     implementation("org.hibernate.common:hibernate-commons-annotations:6.0.6.Final")
     implementation("org.hibernate.validator:hibernate-validator:8.0.1.Final")
     implementation("org.hibernate.validator:hibernate-validator-annotation-processor:8.0.1.Final")
-    implementation("org.mariuszgromada.math:MathParser.org-mXparser:5.2.1")
-    implementation("org.projectlombok:lombok:1.18.28")
-    implementation("org.reflections:reflections:0.10.2")
+    // Log
     implementation("org.slf4j:slf4j-api:2.0.9")
+    implementation("ch.qos.logback:logback-classic:1.4.14")
+    implementation("ch.qos.logback:logback-core:1.4.14")
     implementation("org.threeten:threetenbp:1.6.8")
     implementation("org.yaml:snakeyaml:2.0")
-    implementation("pw.chew:jda-chewtils:2.0-SNAPSHOT")
+    // Spring
     implementation("org.springframework.boot:spring-boot-starter-actuator:${springBootVersion}")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:${springBootVersion}")
     implementation("org.springframework.boot:spring-boot-starter-validation:${springBootVersion}")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor:${springBootVersion}")
-    runtimeOnly("org.mariadb.jdbc:mariadb-java-client:3.3.2")
     developmentOnly("org.springframework.boot:spring-boot-devtools:${springBootVersion}")
+    // MariaDB
+    runtimeOnly("org.mariadb.jdbc:mariadb-java-client:3.3.2")
 
+    // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test:${springBootVersion}") {
         exclude(group = "com.vaadin.external.google", module = "android-json")
     }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,5 @@
 rootProject.name = "DacoBot"
+
+plugins {
+    id("de.fayard.refreshVersions") version "0.60.4"
+}
